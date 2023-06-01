85b54774b483c2fb0343bb1fb41a9ffc0d0b77ca
AndronikosN <129687926+AndronikosN@users.noreply.github.com>
2023-06-01 12:30:18 +0300

    Deploy changes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import {Routes, Route} from 'react-router-dom';
 import Login from "./components/Login";
 import Cats from "./components/Cats";
 import Favorites from "./components/Favorites";
+import Box from "@mui/material/Box";
+
 
 function App() {
   return (
-    <>
+    <Box>
     <Routes>
       <Route path='/' element={<Login />}></Route>
       <Route path='cats' element={<Cats />}></Route>
       <Route path='favorites' element={<Favorites />}></Route>
     </Routes>
-    </>
+    </Box>
   );
 }
 
